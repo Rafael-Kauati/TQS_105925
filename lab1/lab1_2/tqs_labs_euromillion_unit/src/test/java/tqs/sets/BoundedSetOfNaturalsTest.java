8e04240f02caf7d9e946c83c46e6f646d34ec121
TK <kauati.toiti@gmail.com>
2024-02-19 10:29:20 +0000

    lab 1.2 , ex 2d

--- lab1/lab1_2/tqs_labs_euromillion_unit/src/test/java/tqs/sets/BoundedSetOfNaturalsTest.java
+++ lab1/lab1_2/tqs_labs_euromillion_unit/src/test/java/tqs/sets/BoundedSetOfNaturalsTest.java
@@ -23,7 +23,7 @@ class BoundedSetOfNaturalsTest {
 
     @BeforeEach
     public void setUp() {
-        setA = new BoundedSetOfNaturals(1);
+        setA = new BoundedSetOfNaturals(5);
         setB = BoundedSetOfNaturals.fromArray(new int[]{10, 20, 30, 40, 50, 60});
         setC = BoundedSetOfNaturals.fromArray(new int[]{50, 60});
     }
@@ -33,21 +33,22 @@ class BoundedSetOfNaturalsTest {
         setA = setB = setC = null;
     }
 
-    @DisplayName("TODO revise test logic")
+    @DisplayName("\n === Add and Size === \n")
     @Test
     public void testAddElement() {
 
-        setA.add(99);
-        assertTrue(setA.contains(99), "add: added element not found in set.");
-        assertEquals(1, setA.size());
+        setA.add(5);
+        System.out.println(setA.size());
+        assertTrue(setA.contains(5), "add: added element not found in set.");
+        assertEquals(1, setA.size(), "add: elements count not as expected.");
 
-        setB.add(11);
-        assertTrue(setB.contains(11), "add: added element not found in set.");
-        assertEquals(7, setB.size(), "add: elements count not as expected.");
+        assertThrows(IllegalArgumentException.class, () -> setA.add(5), "add: adding element should have throw an IllegalArgumentException.");
+        assertEquals(1, setA.size(), "add: elements count not as expected.");
 
-        assertThrows(IllegalArgumentException.class, () -> setB.add(11), "add: adding element should have throw an IllegalArgumentException.");
-        assertFalse(setB.contains(11), "add: this element should not have been found on the set");
-        assertEquals(6, setB.size(), "add: elements count not as expected.");
+        assertThrows(IllegalArgumentException.class, () -> setA.add(-5), "add: adding element should have throw an IllegalArgumentException.");
+        assertEquals(1, setA.size(), "add: elements count not as expected.");
+
+
     }
 
     //@Disabled("TODO revise to test the construction from invalid arrays")
@@ -62,6 +63,7 @@ class BoundedSetOfNaturalsTest {
                 (Executable) () -> assertThrows(IllegalArgumentException.class, () -> setA.add(set1)) ,
                 (Executable) () -> assertThrows(IllegalArgumentException.class, () -> setA.add(set2))
         );
+
     }
 
     /*
