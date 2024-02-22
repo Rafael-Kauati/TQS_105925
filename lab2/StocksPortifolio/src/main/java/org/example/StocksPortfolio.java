281e07619d6e107568d0caaeff263ffac2f1ba4d
TK <kauati.toiti@gmail.com>
2024-02-22 16:04:33 +0000

    lab 2,1 done till b)

--- lab2/StocksPortifolio/src/main/java/org/example/StocksPortfolio.java
+++ lab2/StocksPortifolio/src/main/java/org/example/StocksPortfolio.java
@@ -1,16 +1,23 @@
 package org.example;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
-@RequiredArgsConstructor
+@NoArgsConstructor  // Add this annotation to generate a no-args constructor
 @Data
 public class StocksPortfolio {
 
-    private final Collection<Stock> stocks ;
-    private final IStockmarketService market;
+    private  Collection<Stock> stocks ;
+    private  IStockmarketService market;
+
+    public StocksPortfolio(IStockmarketService market) {
+        this.stocks = new ArrayList<>();
+        this.market = market;
+    }
 
     public void newStock(Stock s){ stocks.add(s); }
 
--- lab2/StocksPortifolio/src/test/java/StockPortfolioTest.java
+++ lab2/StocksPortifolio/src/test/java/StockPortfolioTest.java
@@ -1,18 +1,26 @@
 import org.example.IStockmarketService;
 import org.example.Stock;
 import org.example.StocksPortfolio;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.verification.VerificationMode;
 
-import static org.mockito.Mockito.when;
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
 public class StockPortfolioTest
 {
+
+
         @Mock
         private IStockmarketService service;
 
@@ -20,18 +28,26 @@ public class StockPortfolioTest
         @InjectMocks
         private StocksPortfolio portfolio;
 
-
         @Test
-        void TestTotalValeu()
+        void TestTotalValue()
         {
             when(service.lookUpPrice("STEAM")).thenReturn(4.0);
             when(service.lookUpPrice("NVIDEA")).thenReturn(1.5);
+            /*
+            * Start the test with the mock remote instance
+            */
 
 
+            portfolio.newStock(new Stock("STEAM", 4));
+            portfolio.newStock(new Stock("NVIDEA", 2));
 
+            final double total  = portfolio.totalValue();
+            assertEquals(19.0, total);
 
-
+            // 5. Verify the result (assert) and the use of the mock (verify)
+            verify(service, times(2)).lookUpPrice(anyString());
 
         }
+
 }
 
