a4b43dad2d741434ad9c1510458b7a553c233805
TK <kauati.toiti@gmail.com>
2024-04-04 17:26:39 +0100

    Test : Book a Reservation for a passenge

--- HW1/backend/src/test/java/deti/traveler/controller/ControllerTest.java
+++ HW1/backend/src/test/java/deti/traveler/controller/ControllerTest.java
@@ -2,6 +2,7 @@ package deti.traveler.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import deti.traveler.entity.Model.TravelModel;
+import deti.traveler.entity.Ticket;
 import deti.traveler.entity.Travel;
 import deti.traveler.service.TravelService;
 import deti.traveler.service.utils.CURRENCY;
@@ -19,7 +20,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.io.IOException;
@@ -27,7 +28,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -46,7 +46,13 @@ class ControllerTest
     @MockBean
     private TravelService service;
 
-    private final Travel DummyTravel = new Travel(1L, "Dublin, Ireland","Galway, Ireland",LocalDateTime.now(),  LocalDateTime.now(), 2, 11.99);
+    private final Travel DummyTravel = new Travel(1L, "Dublin, Ireland","Galway, Ireland",LocalDateTime.now(),  LocalDateTime.now(), 6, 11.99);
+
+    private final Ticket DummyTicket = Ticket.builder()
+            .owner("James Lee")
+            .travel(DummyTravel)
+            .purchasedAt(LocalDateTime.now())
+            .build();
 
 
     @Test
@@ -65,11 +71,32 @@ class ControllerTest
     }
 
 
+    @Test
+    void TestPurchaseTicketForATravel() throws Exception {
+
+        //'http://localhost:9090/purchase/1?owner=JohnDoe&numSeatsBooked=2'
+
+        mockController.perform(get("/purchase/1")
+                        .param("owner", "JohnDoe")
+                        .param("numSeatsBooked", "2")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+
+
 
     @BeforeEach
     void setupTest() throws IOException, InterruptedException {
         final List<Travel> trips = new ArrayList<>();
         trips.add(DummyTravel);
+
+        when(service.purchaseTicket(
+                eq(DummyTravel.getId()),
+                eq(DummyTicket.getOwner()),
+                anyInt()
+        )).thenReturn(DummyTicket);
 
         when(service.getTravel(
                 eq(DummyTravel.getFromcity()),
